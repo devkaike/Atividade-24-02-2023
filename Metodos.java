e6c38051bdf80b8edcb402abb548ed3ab6834f70
Kaike silva <107559196+devkaike@users.noreply.github.com>
2023-02-28 15:49:52 -0300

    Update Metodos.java

--- Metodos.java
+++ Metodos.java
@@ -50,7 +50,7 @@ public class Metodos {
         int dia, mes, ano;
         dia = r.nextInt(31)+1;
         mes = r.nextInt(12)+1;
-        ano = r.nextInt(42)+1981;
+        ano = r.nextInt(22)+1981;
         if(mes == 2 && dia > 30){
             geraDt();
         }
@@ -93,7 +93,7 @@ public class Metodos {
         num = r.nextInt(26)+ 65;
         l = (char)num;
         sobreNome += l;
-        for(int i = 0; i < 9; i++) {
+        for(int i = 0; i < 8; i++) {
             num = r.nextInt(26) + 97;
             l = (char)num;
             sobreNome += l;
@@ -102,102 +102,110 @@ public class Metodos {
         num = r.nextInt(26)+ 65;
         l = (char)num;
         sobreNome2 += l;
-        for(int i = 0; i < 9; i++) {
+        for(int i = 0; i < 8; i++) {
             num = r.nextInt(26) + 97;
             l = (char)num;
             sobreNome2 += l;
         }
         return nom +" "+ sobreNome +" "+ sobreNome2;
     } 
-    public String manipulaCod(int cod){
-        String seq = "";
-        
-        if(cod < 10){
-            seq = "00"+cod;
-        }else{
-            if(cod < 100){
-                seq = "0"+cod;
-            }else{
-                seq = ""+cod;
-            }
+    public String manipulaCod(int cod, int tamSeq){  
+        String newSeq = "";
+        for(int i = 0; i < tamSeq; i++){
+            newSeq += "0";
         }
+       
+        newSeq += ""+cod;
         cod++;
-        //int aux = cod;
-        return seq;
+        return newSeq;
     }
-    public String pegaProfissao(){
+    String prof1[] = new String[8];
+    public String pegaProfissao(int cod, int aux2){
+
         String prof[] = {"Engenheiro", "Médico", "Advogado", "Professor", "Programador", "Arquiteto", "Jornalista",
         "Farmacêutico", "Enfermeiro", "Dentista", "Psicólogo", "Fisioterapeuta", "Veterinário",
         "Nutricionista", "Designer", "Administrador", "Músico", "Ator", "Biólogo", "Geólogo"};
-        return prof[r.nextInt(20)];
-    }
-
-    public String geraDep(int cod){
-        String dep = "";
-        if(cod == 0){
-            dep = "<Sem dependentes> \n";
-            return dep;
-        }else{
-            if(cod >= 1){
-                for(int a = 0; a < cod; a++){
-                    String nom = "";
+        
+        int aux[] = new int[8];
+        if(cod == aux2){
+            aux[0] = r.nextInt(20);
+            prof1[0] = prof[aux[0]];
+            for(int i = 1; i < 8; i++){
+                aux[i] = r.nextInt(20);
+                prof1[i] = prof[aux[i]];
+                for(int j = 0; j < i; j++){
+                    if(aux[i] == aux[j]){
+                        i--;
+                    }
+                }
             
-                    char l;
-                    int num;
-                    num = r.nextInt(26)+ 65;
-                    l = (char)num;
-                    nom += l;
-                    for(int i = 0; i < 9; i++) {
-                        num = r.nextInt(26) + 97;
-                        l = (char)num;
-                        nom += l;
-                    }
-                    String sobreNome = " ";
-                    num = r.nextInt(26)+ 65;
-                    l = (char)num;
-                    sobreNome += l;
-                    for(int i = 0; i < 9; i++) {
-                        num = r.nextInt(26) + 97;
-                        l = (char)num;
-                        sobreNome += l;
-                    }
-                    dep += "  "+nom + sobreNome;
-                    int dia, mes, ano;
-                    dia = r.nextInt(31)+1;
-                    mes = r.nextInt(12)+1;
-                    ano = r.nextInt(42)+1981;
-                    if(mes == 2 && dia > 30){
-                        geraDt();
-                    }
-                    if(ano % 4 > 0 && mes == 2 && dia > 28){
-                        geraDt();
-                    }
-                    if((mes == 11 && dia == 31) && (mes == 9 && dia == 31) && (mes == 6 && dia == 31) && (mes == 4 && dia == 31)){
-                        geraDt();
-                    }
-                    String newMes = "", newDia = "";
-                    if(dia < 10){
-                        newDia = "0"+dia;
-                    }else{
-                        newDia = ""+dia;
-                    }
-                    if(mes < 10){
-                        newMes = "0"+mes;
-                    }else{
-                        newMes = ""+mes;
-                    }
-                    String formatDate = "";
-                    formatDate = newDia +"/"+ newMes +"/"+ ""+ano; 
-                    dep += " "+formatDate;
-                    boolean tipo;
-                    if(tipo = r.nextBoolean()){
-                        dep += "   Conjuge \n";
-                    }else{
-                        dep += "   Filho \n";
-                    }
-                }  
             }
         }
-    return dep;
+        
+            
+        return prof1[r.nextInt(8)];
+    }
+    public String geraNomeDep(){
+        String nom = "";
+            
+        char l;
+        int num;
+        num = r.nextInt(26)+ 65;
+        l = (char)num;
+        nom += l;
+        for(int i = 0; i < 9; i++) {
+            num = r.nextInt(26) + 97;
+            l = (char)num;
+            nom += l;
+        }
+        String sobreNome = "";
+        num = r.nextInt(26)+ 65;
+        l = (char)num;
+        sobreNome += l;
+        for(int i = 0; i < 8; i++) {
+            num = r.nextInt(26) + 97;
+            l = (char)num;
+            sobreNome += l;
+        }
+        
+        return nom +" "+ sobreNome;
+    }
+    public String geraDtDep(){
+        int dia, mes, ano;
+        dia = r.nextInt(31)+1;
+        mes = r.nextInt(12)+1;
+        ano = r.nextInt(7)+2008;
+        if(mes == 2 && dia > 30){
+            geraDt();
+        }
+        if(ano % 4 > 0 && mes == 2 && dia > 28){
+            geraDt();
+        }
+        if((mes == 11 && dia == 31) && (mes == 9 && dia == 31) && (mes == 6 && dia == 31) && (mes == 4 && dia == 31)){
+            geraDt();
+        }
+        String newMes = "", newDia = "";
+        if(dia < 10){
+            newDia = "0"+dia;
+        }else{
+            newDia = ""+dia;
+        }
+        if(mes < 10){
+            newMes = "0"+mes;
+        }else{
+            newMes = ""+mes;
+        }
+        String formatDate = "";
+        formatDate = newDia +"/"+ newMes +"/"+ ""+ano;
+        return formatDate;
+    }
+    public String geraTipoDep(){
+        int[] n = {1,2,3,4,5};
+        int tam = r.nextInt(5);
+        if(n[tam] != 5){
+            return "Filho";
+        }else{
+            return "conjuge";
+        }
     }
 }

e86492104a53dc86eb28efc432a06f7a7984d69f
Kaike silva <107559196+devkaike@users.noreply.github.com>
2023-02-28 15:52:03 -0300

    Update dependentes.java

--- dependentes.java
+++ dependentes.java
@@ -1,12 +1,27 @@
 public class dependentes{
     private String nomeDep;
+    private String tipo;
+    private String dataDep;
+
+    public dependentes(String nomeDep, String dataDep, String tipo) {
+        this.nomeDep = nomeDep;
+        this.dataDep = dataDep;
+        this.tipo = tipo;
+    }
+    public dependentes(){}
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
     public String getNomeDep() {
         return nomeDep;
     }
     public void setNomeDep(String nomeDep) {
         this.nomeDep = nomeDep;
     }
-    private String dataDep;
     public String getDataDep() {
         return dataDep;
     }
